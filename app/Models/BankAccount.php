3d11fd684796c690c60678585b541da4f9b75460
mohammadrezam <mardani93.itsu@gmail.com>
2022-07-15 17:16:57 +0430

    fix: add relation to BankAccount model

--- app/Models/BankAccount.php
+++ app/Models/BankAccount.php
@@ -4,11 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class BankAccount extends Model
 {
     use HasFactory;
 
     protected $fillable = ['user_id', 'amount'];
-    
+
+    /**
+     * Get the user for the bank accounts.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
