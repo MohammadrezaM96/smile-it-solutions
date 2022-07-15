49404b181d4a443b03aa782ce202b65a48e2d608
mohammadrezam <mardani93.itsu@gmail.com>
2022-07-15 19:49:15 +0430

    fix: Updated Transfer Model

--- app/Models/Transfer.php
+++ app/Models/Transfer.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use App\Contracts\Scopes\TransferScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Transfer extends Model
 {
-    use HasFactory;
+    use HasFactory, TransferScope;
 
     const STATUS_SUCCESS = 'success';
     const STATUS_FAILED = 'failed';
