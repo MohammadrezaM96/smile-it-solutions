3e546a97fd0b13132e3f039101ec4ab940acd38f
mohammadrezam <mardani93.itsu@gmail.com>
2022-07-15 17:16:06 +0430

    feat: Added BankAccountResource

---
+++ app/Http/Resources/V1/Api/BankAccount/BankAccountResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources\V1\Api\BankAccount;
+
+use App\Http\Resources\V1\Api\User\UserResource;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BankAccountResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            '_type' => class_basename($this->resource),
+            'id' => $this->resource->id,
+            'user' => new UserResource($this->resource->user),
+            'balance' => $this->resource->amount,
+            'created_at' => $this->resource->created_at->timestamp,
+        ];
+    }
+}
