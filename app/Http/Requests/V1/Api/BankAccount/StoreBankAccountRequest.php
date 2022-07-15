cb6f3ae9681e23cb2189c10ca8e35aaace7f8d5c
mohammadrezam <mardani93.itsu@gmail.com>
2022-07-15 16:54:13 +0430

    feat: Added Bank Account Request

---
+++ app/Http/Requests/V1/Api/BankAccount/StoreBankAccountRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\V1\Api\BankAccount;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreBankAccountRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'user_id' => ['required' , 'exists:users,id'],
+            'amount' => ['nullable' , 'numeric']
+        ];
+    }
+}
