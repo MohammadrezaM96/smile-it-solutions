c375b4fe8af675c3d22e651bd9ffd9e1e5d4551f
mohammadrezam <mardani93.itsu@gmail.com>
2022-07-15 19:00:27 +0430

    feat: Added Transfer Model

--- app/Models/Transfer.php
+++ app/Models/Transfer.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Transfer extends Model
 {
@@ -15,5 +16,16 @@ class Transfer extends Model
         self::STATUS_SUCCESS, self::STATUS_FAILED
     ];
 
-    protected $fillable = ['origin_id' , 'destination_id', 'status'];
+    protected $fillable = ['origin_id', 'destination_id', 'amount', 'status'];
+
+
+    public function originAccount(): BelongsTo
+    {
+        return $this->belongsTo(BankAccount::class, 'origin_id');
+    } 
+    
+    public function DestinationAccount(): BelongsTo
+    {
+        return $this->belongsTo(BankAccount::class, 'destination_id');
+    }
 }
