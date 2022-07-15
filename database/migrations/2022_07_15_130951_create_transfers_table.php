e138d90fb6d67778ad50f014c48118c7721fac7a
mohammadrezam <mardani93.itsu@gmail.com>
2022-07-15 18:05:58 +0430

    feat: Added Transfer Migration

---
+++ database/migrations/2022_07_15_130951_create_transfers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transfers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('origin_id');
+            $table->foreignId('destination_id');
+            $table->unsignedBigInteger('amount');
+            $table->enum('status', \App\Models\Transfer::STATUSES)->default(\App\Models\Transfer::STATUS_SUCCESS);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transfers');
+    }
+};
