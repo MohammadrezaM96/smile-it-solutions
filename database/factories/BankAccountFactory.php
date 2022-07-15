a875d3b7083f3660222781fc952dfccd8ba5e4ba
mohammadrezam <mardani93.itsu@gmail.com>
2022-07-15 16:17:58 +0430

    feat: Added BankAccount Factory

---
+++ database/factories/BankAccountFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\BankAccount>
+ */
+class BankAccountFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => $this->faker->numberBetween(1, User::count()),
+            'amount' => $this->faker->numberBetween(10000, 200000)
+        ];
+    }
+}
