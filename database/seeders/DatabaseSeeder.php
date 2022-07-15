520c720362fc893971dc387048ffd4fb29642066
mohammadrezam <mardani93.itsu@gmail.com>
2022-07-15 18:06:23 +0430

    fix: Updated Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            BankAccountSeeder::class
+            BankAccountSeeder::class,
+            TransferSeeder::class
         ]);
     }
 }
