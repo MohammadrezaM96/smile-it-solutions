9a50e6a4fc791d8b141c53b2840364fd944e6c91
mohammadrezam <mardani93.itsu@gmail.com>
2022-07-15 19:01:05 +0430

    feat: Added TransferRepository

---
+++ app/Repositories/Eloquent/TransferRepository.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Repositories\Eloquent;
+
+use App\Models\BankAccount;
+use App\Models\Transfer;
+use App\Repositories\Interfaces\TransferRepositoryInterface;
+use Exception;
+
+class TransferRepository implements TransferRepositoryInterface
+{
+
+    public function find($id)
+    {
+        return Transfer::find($id);
+    }
+
+    public function getAll()
+    {
+        return Transfer::all();
+    }
+
+    public function create($data)
+    {
+        try {
+            $bankAccountRepository = new BankAccountRepository();
+            $origin = $bankAccountRepository->find($data['origin_id']);
+            $destination = $bankAccountRepository->find($data['destination_id']);
+
+            $originAmount = $origin->amount - $data['amount'];
+            $destinationAmount = $destination->amount + $data['amount'];
+
+            throw_if($originAmount < 0, false);
+
+            $bankAccountRepository->update($data['origin_id'], ['amount' => $originAmount]);
+            $bankAccountRepository->update($data['destination_id'], ['amount' => $destinationAmount]);
+
+            return Transfer::create($data);
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+}
